ddf96093d12b36441caee1a647cbbb2f29c41bca
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-01-24 15:53:48 -0300

    feat: add resuzable panels

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,22 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { Group, Panel, Separator } from "react-resizable-panels";
 
 export const Route = createFileRoute("/")({
-  component: Index,
+  component: Home,
 });
 
-function Index() {
+function Home() {
   return (
-    <div className="p-2">
-      <h3>Welcome Home!</h3>
+    <div className="h-screen bg-zinc-900">
+      <Group orientation="horizontal">
+        <Panel defaultSize="20%" minSize="15%" maxSize="40%">
+          sidebar
+        </Panel>
+        <Separator className="w-px bg-zinc-700 transition-colors duration-150 hover:bg-zinc-600" />
+        <Panel defaultSize="80%" minSize="60%" maxSize="75%">
+          content
+        </Panel>
+      </Group>
     </div>
   );
 }
