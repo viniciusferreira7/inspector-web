0069c5aa36e2422f5becc86ef8c9e2298dd7e5ca
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-03-07 15:28:12 -0300

    feat: add env schema validation with zod

--- src/components/webhooks-list.tsx
+++ src/components/webhooks-list.tsx
@@ -1,6 +1,20 @@
+import { useQuery } from "@tanstack/react-query";
+import { env } from "../env";
+import { webhookListSchema } from "../http/schemas/webhooks";
 import { WebHooksListItem } from "./webhooks-list-item";
 
 export function WebhooksList() {
+  const { data: webhooks, isLoading } = useQuery({
+    queryKey: ["webhooks"],
+    queryFn: async () => {
+      const response = await fetch(`${env.VITE_API_URL}/webhooks`);
+      console.log({ response });
+      return webhookListSchema.parse(response.json());
+    },
+  });
+
+  console.log({ webhooks });
+
   return (
     <div className="flex-1 overflow-y-auto">
       <div className="space-y-1 p-2">
