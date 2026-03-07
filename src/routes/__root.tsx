969858054662a2a73d02d4ead46f6f88be1f1baf
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-03-07 15:28:07 -0300

    feat: setup react query client

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,12 +1,14 @@
+import "@/globals.css";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
-import "@/globals.css";
+import { queryClient } from "../lib/query-client";
 
 const RootLayout = () => (
-  <>
+  <QueryClientProvider client={queryClient}>
     <Outlet />
     <TanStackRouterDevtools />
-  </>
+  </QueryClientProvider>
 );
 
 export const Route = createRootRoute({ component: RootLayout });
